035bbddea77612168ff2dbef351d23ceba29787d
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-03-16 18:50:44 +0530

    feat(learner wrapper): implemented the Sticky wrapper to show the user progress and created the feed wrappper to show the header.

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,22 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./_components/header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
   return (
-    <div>
-      <h1> Hello</h1>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 };
