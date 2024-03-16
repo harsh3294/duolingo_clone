1c9ba03e8848a8d07275ea43d850848f60276a6c
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-03-16 13:28:48 +0530

    fix(footer): reduced the redudant code by looping the array of objects.

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -2,59 +2,43 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export const Footer = () => {
+  const languages = [
+    {
+      name: "Croatian",
+      flagPath: "/hr.svg",
+    },
+    {
+      name: "Spanish",
+      flagPath: "/es.svg",
+    },
+    {
+      name: "French",
+      flagPath: "/fr.svg",
+    },
+    {
+      name: "Italian",
+      flagPath: "/it.svg",
+    },
+    {
+      name: "Japanese",
+      flagPath: "/jp.svg",
+    },
+  ];
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
       <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
-        <Button size="lg" variant="ghost" className="w-full">
-          <Image
-            src="/hr.svg"
-            alt="Croatian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Croatian
-        </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <Image
-            src="/es.svg"
-            alt="Spanish"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Spanish
-        </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <Image
-            src="/fr.svg"
-            alt="French"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          French
-        </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <Image
-            src="/it.svg"
-            alt="Italian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Italian
-        </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <Image
-            src="/jp.svg"
-            alt="Japanese"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Japanese
-        </Button>
+        {languages.map((language) => (
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src={language.flagPath}
+              alt={language.name}
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            {language.name}
+          </Button>
+        ))}
       </div>
     </footer>
   );
