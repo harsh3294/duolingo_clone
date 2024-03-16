d78716f9e20e4394f0f621a1e3d8b7c624c244b1
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-03-16 12:27:48 +0530

    feat(button): updated the button variants.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const font = Nunito({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Duolingo - The world's best way to learn a language",
   description:
-    "With our free mobile app or web and a few minutes a day, everyone can Duolingo. Learn 30+ languages online with bite-size lessons based on science.",
+    "Learn 30+ languages online with bite-size lessons based on science.",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Duolingoo</div>
+    <div>
+      <Button> Hellow </Button>
+    </div>
   );
 }
