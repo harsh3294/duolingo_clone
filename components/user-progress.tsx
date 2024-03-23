f0e7c9eb00292b332b1fcd54141d672aa07ee7f3
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-03-23 11:00:40 +0530

    fix(header): dynamically fetching  the user progress.

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
-
+import { courses } from "@/db/schema";
 import { Button } from "@/components/ui/button";
 
 type Props = {
-  activeCourse: any;
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
