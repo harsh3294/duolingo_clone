e19225224f21e12d33c3822e376897fea821af6a
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-03-23 10:35:36 +0530

    feat(route loading): added the loading indicator at the top of the screen when ever the route is changing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import NextTopLoader from "nextjs-toploader";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -21,6 +22,21 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={font.className}>
+          <NextTopLoader
+            color="rgb(22 163 74 / 1)"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={4}
+            crawl={true}
+            showSpinner={true}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px rgb(22 163 74 / 1),0 0 5px rgb(22 163 74 / 1)"
+            template='<div class="bar" role="bar"><div class="peg"></div></div>
+            <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+            zIndex={1600}
+            showAtBottom={false}
+          />
           <Toaster />
           {children}
         </body>
