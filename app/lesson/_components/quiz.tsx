5594d166e3081f2297853d2dac82ffaf3b4bd78e
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-03-25 11:36:46 +0530

    feat(audio): added the different audio for the correct and incorrect answer.

--- app/lesson/_components/quiz.tsx
+++ app/lesson/_components/quiz.tsx
@@ -9,6 +9,7 @@ import { Footer } from "./footer";
 import { upsertChallengeProgress } from "@/actions/challenge-progress";
 import { toast } from "sonner";
 import { reduceHearts } from "@/actions/user-progress";
+import { useAudio } from "react-use";
 
 type Props = {
   initialPercentage: number;
@@ -28,6 +29,11 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const [correctAudio, _c, correctControls] = useAudio({ src: "/correct.wav" });
+  const [incorrectAudio, _i, incorrectControls] = useAudio({
+    src: "/incorrect.wav",
+  });
+
   const [pending, startTransition] = useTransition();
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(() => {
@@ -87,6 +93,7 @@ export const Quiz = ({
             if (response?.error === "hearts") {
               return;
             }
+            correctControls.play();
 
             setStatus("correct");
             setPercentage((prev) => prev + 100 / challenges.length);
@@ -106,6 +113,7 @@ export const Quiz = ({
               return;
             }
 
+            incorrectControls.play();
             setStatus("wrong");
 
             if (!response?.error) {
@@ -124,6 +132,8 @@ export const Quiz = ({
 
   return (
     <>
+      {incorrectAudio}
+      {correctAudio}
       <Header
         hearts={hearts}
         percentage={percentage}
